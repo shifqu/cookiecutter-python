289dec32a4c1c0b1fca4cb034fe28711fdae029e
Sonny V <sonny@softllama.net>
2021-05-01 17:01:14 +0200

    build(lint.sh): Modify the order in which the scripts are executed
    Mypy can take a while, so we check other things (that take less time to execute) before executing mypy
    
    Closes #3

--- {{cookiecutter.project_name}}/scripts/lint.sh
+++ {{cookiecutter.project_name}}/scripts/lint.sh
@@ -2,12 +2,12 @@
 set -euxo pipefail
 
 poetry run cruft check
-poetry run mypy --ignore-missing-imports {{cookiecutter.project_name}}/
+poetry run find . -not -path '*/\.*' -not -path '*/__*' -not -path '*/tests/*' -type f \( -iname "*md" -o -iname "*txt" \) \ | xargs proselint
+poetry run find scripts/ -type f -not -name '*py' | xargs shellcheck
 poetry run isort --check --diff {{cookiecutter.project_name}}/ tests/
 poetry run black --check {{cookiecutter.project_name}}/ tests/
-poetry run flake8 {{cookiecutter.project_name}}/ tests/
-poetry run safety check -i 39462
-poetry run bandit -r {{cookiecutter.project_name}}/
 poetry run pydocstyle {{cookiecutter.project_name}}/ tests/
-poetry run find . -not -path '*/\.*' -not -path '*/__*' -not -path '*/tests/*' -type f \( -iname "*md" -o -iname "*txt" \) | xargs proselint
-poetry run find scripts/ -type f -not -name '*py' | xargs shellcheck
+poetry run flake8 {{cookiecutter.project_name}}/ tests/
+poetry run mypy --ignore-missing-imports {{cookiecutter.project_name}}/
+poetry run bandit -r {{cookiecutter.project_name}}/
+poetry run safety check
