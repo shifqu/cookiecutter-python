5dd5bb3474292f73a70fff0d9f0e9b4dbeb15ca9
Sonny V <sonny@softllama.net>
2021-05-04 10:48:43 +0200

    style: Update docstrings to use the numpy format instead of google's

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__main__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__main__.py
@@ -1,9 +1,8 @@
 """The main file that is executed when this project is run as a module.
 
-Typical usage example::
-
-    python -m module_name --version
-
+Examples
+--------
+python -m module_name --version
 """
 from .main import main
 
