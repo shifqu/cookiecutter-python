7dc23febe28e518c8d997e1392be302cc4f61c40
Sonny V <sonny@softllama.net>
2021-05-10 09:11:24 +0200

    build(scripts): modify install script to be more robust, but do not use option -x

--- {{cookiecutter.project_name}}/scripts/install.sh
+++ {{cookiecutter.project_name}}/scripts/install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
-poetry install
-pre-commit install -t pre-commit -t pre-push
+if poetry install; then
+    echo 'successfully installed poetry env'
+    if poetry run pre-commit install -t pre-commit -t pre-push; then
+        echo 'successfully installed pre-commit hooks'
+        echo 'use "poetry shell" to activate the new virtual environment'
+        echo 'more info: https://python-poetry.org/docs/basic-usage/#activating-the-virtual-environment'
+    else
+        echo 'fatal: could not install pre-commit'
+    fi
+else
+    echo 'fatal: "poetry install" failed'
+fi
