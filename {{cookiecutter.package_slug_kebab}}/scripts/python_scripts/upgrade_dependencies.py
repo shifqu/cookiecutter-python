daae833cd4386f240fc69ae215e508725ea833cc
Sonny V <sonny@softllama.net>
2022-01-25 18:20:16 +0100

    fix(python_scripts): add typing to and slightly refactor upgrade_dependencies.py

--- {{cookiecutter.package_slug_kebab}}/scripts/python_scripts/upgrade_dependencies.py
+++ {{cookiecutter.package_slug_kebab}}/scripts/python_scripts/upgrade_dependencies.py
@@ -1,29 +1,37 @@
-
 """Upgrade the package's current dependencies.
 
 NOTE: This upgrades the dependencies outside their specified boundaries.
 """
 import subprocess
+from collections.abc import MutableMapping
 from pathlib import Path
+from typing import Any, Optional
 
 import toml
 
 
-def _get_poetry_add_latest_command(packages, dev=False):
-    command = ["poetry", "add"]
+def _toml_file(path: Optional[Path] = None) -> MutableMapping[str, Any]:
+    if path is None:
+        path = Path.cwd()
+    path_glob = path.glob("pyproject.toml")
+    current_toml = next(path_glob, None)
+    if current_toml is None:
+        raise FileNotFoundError(f"pyproject.toml was not found in {path}")
+    return toml.load(current_toml)
+
+
+def _poetry_add_latest(packages: MutableMapping[str, Any], dev: bool = False) -> None:
     if dev:
-        command.append("--dev")
-    for pkg_name in packages:
-        add = f"{pkg_name}@latest"
-        command.append(add)
-    return command
+        initial_command = ["poetry", "add", "--dev"]
+    else:
+        initial_command = ["poetry", "add"]
+
+    package_names_latest = [f"{package_name}@latest" for package_name in packages]
+    final_command = initial_command + package_names_latest
+    subprocess.run(final_command)
 
 
-def _run_command(cmd: list):
-    subprocess.run(cmd)
-
-
-def main():
+def main() -> None:
     """Upgrade the poetry-managed project's dependencies.
 
     Raises
@@ -31,19 +39,13 @@ def main():
     FileNotFoundError
         Whenever no pyproject.toml is found recursively in `cwd`.
     """
-    current_toml = next(Path.cwd().glob("pyproject.toml"), None)
-    if not current_toml:
-        raise FileNotFoundError(f"pyproject.toml was not found in {Path.cwd()}")
-    pyproject_toml = toml.load(current_toml)
-
-    dependencies = pyproject_toml["tool"]["poetry"]["dependencies"]
-    dev_dependencies = pyproject_toml["tool"]["poetry"]["dev-dependencies"]
+    pyproject_toml = _toml_file()
+    tool_poetry = pyproject_toml["tool"]["poetry"]
+    dependencies: MutableMapping[str, Any] = tool_poetry["dependencies"]
     dependencies.pop("python", None)
-    cmd_1 = _get_poetry_add_latest_command(dependencies)
-    _run_command(cmd_1)
-    cmd_2 = _get_poetry_add_latest_command(dev_dependencies, dev=True)
-    _run_command(cmd_2)
-    print("done")
+    _poetry_add_latest(dependencies)
+    dev_dependencies = tool_poetry["dev-dependencies"]
+    _poetry_add_latest(dev_dependencies, dev=True)
 
 
 if __name__ == "__main__":
