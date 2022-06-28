6e218a96de93300f14c6ff9e713d088014328199
shifqu <shifqu@softllama.net>
2022-06-28 17:27:04 +0000

    build: remove proselint

--- {{cookiecutter.package_slug_kebab}}/scripts/lint.sh
+++ {{cookiecutter.package_slug_kebab}}/scripts/lint.sh
@@ -2,7 +2,6 @@
 set -euxo pipefail
 
 poetry run cruft check
-find . -not -path '*/\.*' -not -path '*/__*' -not -path '*/tests/*' -type f \( -iname "*md" -o -iname "*txt" \) | poetry run xargs proselint
 find scripts/ -type f -not -name '*py' | poetry run xargs shellcheck
 poetry run isort --check --diff {{cookiecutter.package_slug}}/ tests/
 poetry run black --check {{cookiecutter.package_slug}}/ tests/
